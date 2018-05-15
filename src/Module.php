10b82d8dfe851635947975f639504f5adc17a80b
popovsergiy <popow.sergiy@gmail.com>
2018-05-15 15:22:06 +0300

    ZF3 adapting

--- src/Module.php
+++ src/Module.php
@@ -1,10 +1,15 @@
 <?php
+
 namespace Popov\ZfcDataGridPlugin;
 
 class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/../config/module.config.php';
+        $config = include __DIR__ . '/../config/module.config.php';
+        $config['service_manager'] = $config['dependencies'];
+        unset($config['dependencies']);
+
+        return $config;
     }
 }
